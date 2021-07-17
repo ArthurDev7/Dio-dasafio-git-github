e398fa24dec766e20b8b183130b989b5e2cdda91
ArthurDev7 <arthurluizdev7@gmail.com>
2021-07-17 07:21:16 -0300

    testing commit to study

---
+++ atalhos/src/atalhosTest/atalhosTeclado.java
@@ -0,0 +1,11 @@
+package atalhosTest;
+
+import atalhosTest.model.Gato;
+
+public class atalhosTeclado {
+    public static void main(String[] args) {
+
+        Gato gato = new Gato();
+        System.out.println(gato);
+    }
+}
